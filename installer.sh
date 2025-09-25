e2bf73f36fc444f6eb20c380434e1a7b31172eb5
mujasoft <20296472+mujasoft@users.noreply.github.com>
2025-09-24 22:54:24 -0400

    WIP

--- installer.sh
+++ installer.sh
@@ -20,11 +20,8 @@ fi
 TMP_DIR=$(mktemp -d)
 git clone --depth 1 "$REPO_URL" "$TMP_DIR"
 
-# Make sure script is executable
-chmod +x "$TMP_DIR/gif4docs.sh"
-
 # Copy to /usr/local/bin
-sudo cp "$TMP_DIR/gif4docs" "$TARGET"
+sudo cp "$TMP_DIR/$TARGET" "$TARGET"
 
 # Clean up
 rm -rf "$TMP_DIR"
